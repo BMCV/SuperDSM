36490573ffdddb0862c10df6c7d10899ae634235
Leonid Kostrykin <void@evoid.de>
2018-02-24 01:37:42 +0100

    adds `derive` to `gocell.config`

--- gocell/config.py
+++ gocell/config.py
@@ -1,3 +1,6 @@
+import json
+
+
 def pop_value(kwargs, kw, default):
     return kwargs.pop(kw) if kw in kwargs else default
 
@@ -9,4 +12,20 @@ def set_default_value(kwargs, kw, default):
 def get_value(config, key, default):
     if key not in config: config[key] = default
     return config[key]
+
+
+def update(base_cfg, cfg_override):
+    for key, val in cfg_override.items():
+        if key not in base_cfg.keys() or not isinstance(val, dict):
+            base_cfg[key] = val
+        else:
+            if not isinstance(base_cfg[key], dict):
+                base_cfg[key] = {}
+            update(base_cfg[key], val)
+    return base_cfg
+
+
+def derive(base_cfg, cfg_override):
+    cfg = json.loads(json.dumps(base_cfg))
+    return update(cfg, cfg_override)
 
