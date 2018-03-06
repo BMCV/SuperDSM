9392dbedb382aa69f9e4d71067ee54608b43b068
Leonid Kostrykin <void@evoid.de>
2018-03-06 14:28:08 +0100

    adds `kde_pure` mode to `ThresholdedLabels.compute_threshold`

--- gocell/labels.py
+++ gocell/labels.py
@@ -35,7 +35,7 @@ class ThresholdedLabels:
         return self.g.model - self.t
 
     @staticmethod
-    def compute_kde_threshold(g, bandwidth, samples):
+    def compute_kde_threshold(g, bandwidth, samples, otsu_ub=True):
         """Computes the threshold based on kernel density estimate of the histogram.
         """
         assert bandwidth > 0 and samples > 0
@@ -51,15 +51,11 @@ class ThresholdedLabels:
         z = np.exp(kde.score_samples(h[:, None]))
         z_minima = [t for t in peak_local_max(z.max() - z) if X_mean + h[t] * X_std > 0]
 
-#        from matplotlib import pyplot as plt
-#        plt.figure()
-#        plt.hist(X[np.logical_and(X >= -1, X <= +1)], bins=50)
-#        plt.gca().twinx().plot(h, z, 'r', lw=2)
-#        for t in z_minima: plt.vlines(h[t], plt.ylim()[0], plt.ylim()[1], 'r', lw=2)
-#        plt.show()
-
         thres_otsu  = threshold_otsu(X_raw)
         thres_kde   = np.inf if len(z_minima) == 0 else h[min(z_minima)] * X_std + X_mean
+
+        if not otsu_ub and (X_raw > thres_kde).sum() > 0.1 * np.prod(X_raw.shape):
+            return thres_kde
 
         if thres_kde > thres_otsu: thres_final = thres_otsu
         else:
@@ -67,16 +63,15 @@ class ThresholdedLabels:
                 thres_final = thres_kde
             else:
                 thres_final = thres_otsu
-#        print(thres_kde)
-#        print(thres_final)
         return thres_final
 
     @staticmethod
     def compute_threshold(g, method='otsu', bandwidth=None, samples_count=None):
         if method == 'otsu':
             return threshold_otsu(g.model[g.mask])
-        elif method == 'kde':
-            return ThresholdedLabels.compute_kde_threshold(g, bandwidth, samples_count)
+        elif method == 'kde' or method == 'kde_pure':
+            otsu_ub = (method == 'kde')  ## use Otsu threshold as upperr bound if method is not pure KDE
+            return ThresholdedLabels.compute_kde_threshold(g, bandwidth, samples_count, otsu_ub=otsu_ub)
         else:
             raise ValueError('unknown threshold method "%s"' % method)
 
