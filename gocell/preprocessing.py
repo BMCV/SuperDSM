cc49244beb4ecbaf9a0743cf1977b716952e5f3d
Leonid Kostrykin <void@evoid.de>
2020-07-23 13:09:20 +0200

    adds `threshold_clip` option for `preprocess1`

--- gocell/preprocessing.py
+++ gocell/preprocessing.py
@@ -22,8 +22,23 @@ class PreprocessingStage1(gocell.pipeline.Stage):
 
         sigma1 = gocell.config.get_value(cfg, 'sigma1', math.sqrt(2))
         sigma2 = gocell.config.get_value(cfg, 'sigma2', 40)
+        threshold_clip = gocell.config.get_value(cfg, 'threshold_clip', np.inf)
 
-        y = ndi.gaussian_filter(g_raw, sigma1) - ndi.gaussian_filter(g_raw, sigma2)
+        threshold_original = ndi.gaussian_filter(g_raw, sigma2)
+        if np.isinf(threshold_clip):
+            threshold_combined = threshold_original
+
+        else:
+            threshold_clip_abs = threshold_clip * g_raw.std()
+            threshold_clipped  = ndi.gaussian_filter(g_raw.clip(0, threshold_clip_abs), sigma2)
+
+            clip_area = (g_raw > threshold_clip_abs)
+            _tmp1 = ndi.distance_transform_edt(~clip_area)
+            _tmp1 = (sigma2 - _tmp1).clip(0, np.inf)
+            _tmp1 = (_tmp1 / _tmp1.max()) ** 2
+            threshold_combined = (1 - _tmp1) * threshold_clipped + _tmp1 * threshold_original
+
+        y = ndi.gaussian_filter(g_raw, sigma1) - threshold_combined
         
         return {
             'y': y
