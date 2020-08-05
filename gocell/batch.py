c4405cd091ecd56382f430511aae7ed38fa7542b
Leonid Kostrykin <void@evoid.de>
2020-08-05 04:26:35 +0200

    wip
    Adds `min_contrast_response` option for post-processing and fixes result rendering bug in batch processing.

--- gocell/batch.py
+++ gocell/batch.py
@@ -100,9 +100,15 @@ def __process_file(pipeline, data, im_filepath, seg_filepath, seg_border, log_fi
 
     if seg_filepath is not None:
         if seg_border is None: seg_border = 8
-        candidates = result_data['cover'].solution
-        postprocessed_footprints = frozenset([frozenset(c.original.footprint) for c in result_data['postprocessed_candidates']])
-        colors = {c: ('g' if c.footprint in postprocessed_footprints else 'r') for c in candidates}
+        candidates, colors = [], {}
+        for candidate in result_data['cover'].solution:
+            postprocessed_candidate = [c for c in result_data['postprocessed_candidates'] if c.original is candidate]
+            if len(postprocessed_candidate) > 0:
+                candidates.append(postprocessed_candidate[0])
+                colors[postprocessed_candidate[0]] = 'g'
+            else:
+                candidates.append(candidate)
+                colors[candidate] = 'r'
         im_result = render.render_model_shapes_over_image(result_data, candidates=candidates, border=seg_border, colors=colors)
         aux.mkdir(pathlib.Path(seg_filepath).parents[0])
         io.imwrite(seg_filepath, im_result)
--- gocell/postprocessing.py
+++ gocell/postprocessing.py
@@ -2,6 +2,9 @@ import gocell.pipeline
 import gocell.config
 import gocell.candidates
 
+import scipy.ndimage      as ndi
+import skimage.morphology as morph
+
 import ray
 import math
 import numpy as np
@@ -17,44 +20,58 @@ class Postprocessing(gocell.pipeline.Stage):
                                              outputs = ['postprocessed_candidates'])
 
     def process(self, input_data, cfg, out, log_root_dir):
-        max_energy_rate         = gocell.config.get_value(cfg,         'max_energy_rate', np.inf)
-        discard_image_boundary  = gocell.config.get_value(cfg,  'discard_image_boundary',  False)
-        min_boundary_obj_radius = gocell.config.get_value(cfg, 'min_boundary_obj_radius',      0)
-        min_obj_radius          = gocell.config.get_value(cfg,          'min_obj_radius',      0)
-        max_obj_radius          = gocell.config.get_value(cfg,          'max_obj_radius', np.inf)
-        mask_stdamp             = gocell.config.get_value(cfg,             'mask_stdamp',      0)
-        mask_max_distance       = gocell.config.get_value(cfg,       'mask_max_distance',      0)
-        mask_smoothness         = gocell.config.get_value(cfg,         'mask_smoothness',      3)
+        max_energy_rate         = gocell.config.get_value(cfg,         'max_energy_rate',  np.inf)
+        discard_image_boundary  = gocell.config.get_value(cfg,  'discard_image_boundary',   False)
+        min_boundary_obj_radius = gocell.config.get_value(cfg, 'min_boundary_obj_radius',       0)
+        min_obj_radius          = gocell.config.get_value(cfg,          'min_obj_radius',       0)
+        max_obj_radius          = gocell.config.get_value(cfg,          'max_obj_radius',  np.inf)
+        mask_stdamp             = gocell.config.get_value(cfg,             'mask_stdamp',       2)
+        mask_max_distance       = gocell.config.get_value(cfg,       'mask_max_distance',       0)
+        mask_smoothness         = gocell.config.get_value(cfg,         'mask_smoothness',       3)
+        exterior_scale          = gocell.config.get_value(cfg,          'exterior_scale',       5)
+        min_contrast_response   = gocell.config.get_value(cfg,   'min_contrast_response', -np.inf)
 
-        cover     = input_data['cover']
-        params_id = ray.put(dict(y = input_data['y_surface'], g_atoms = input_data['g_atoms']))
-        futures   = [_measure_candidate.remote(cidx, c, params_id) for cidx, c in enumerate(cover.solution)]
+        params = {
+            'y':                 input_data['y_surface'],
+            'g':                 input_data['g_raw'],
+            'g_atoms':           input_data['g_atoms'],
+            'g_smooth':          ndi.gaussian_filter(input_data['g_raw'], mask_smoothness),
+            'exterior_scale':    exterior_scale,
+            'mask_stdamp':       mask_stdamp,
+            'mask_max_distance': mask_max_distance,
+        }
 
-        if mask_stdamp > 0 and mask_max_distance > 0:
-            _process_mask = _create_mask_postprocessor(input_data['g_raw'], mask_max_distance, mask_stdamp, mask_smoothness)
-        else:
-            _process_mask = lambda candidate: (candidate.fg_offset, candidate.fg_fragment)
+        candidates = [c for c in input_data['cover'].solution if c.fg_fragment.any()]
+        params_id  = ray.put(params)
+        futures    = [_process_candidate.remote(cidx, c, params_id) for cidx, c in enumerate(candidates)]
 
         postprocessed_candidates = []
         for ret_idx, ret in enumerate(gocell.aux.get_ray_1by1(futures)):
-            candidate, measures = cover.solution[ret[0]], ret[1]
-            obj_radius = math.sqrt(candidate.fg_fragment.sum() / math.pi)
-            out.intermediate(f'Post-processing candidates... {ret_idx + 1} / {len(futures)}')
+            candidate, candidate_results = candidates[ret[0]], ret[1]
+            candidate = PostprocessedCandidate(candidate)
 
-            if measures['energy_rate'] > max_energy_rate:
+            if candidate_results['fg_fragment'] is not None and candidate_results['fg_offset'] is not None:
+                candidate.fg_fragment = candidate_results['fg_fragment'].copy()
+                candidate.fg_offset   = candidate_results['fg_offset'  ].copy()
+                if not candidate.fg_fragment.any(): continue
+
+            obj_radius = math.sqrt(candidate.fg_fragment.sum() / math.pi)
+
+            if candidate_results['energy_rate'] > max_energy_rate:
                 continue
-            if candidate.on_boundary:
+            if candidate_results['contrast_response'] < min_contrast_response:
+                continue
+            if candidate.original.on_boundary:
                 if discard_image_boundary or not(min_boundary_obj_radius <= obj_radius < max_obj_radius):
                     continue
             else:
                 if not min_obj_radius <= obj_radius <= max_obj_radius:
                     continue
-            
-            candidate = PostprocessedCandidate(candidate)
-            candidate.fg_offset, candidate.fg_fragment = _process_mask(candidate)
-            postprocessed_candidates.append(candidate)
 
-        out.write(f'Remaining candidates: {len(postprocessed_candidates)} of {len(cover.solution)}')
+            postprocessed_candidates.append(candidate)
+            out.intermediate(f'Post-processing candidates... {ret_idx + 1} / {len(futures)}')
+
+        out.write(f'Remaining candidates: {len(postprocessed_candidates)} of {len(candidates)}')
 
         return {
             'postprocessed_candidates': postprocessed_candidates
@@ -68,12 +85,29 @@ class PostprocessedCandidate(gocell.candidates.BaseCandidate):
         self.fg_fragment = original.fg_fragment
 
 
+def _compute_contrast_response(candidate, g, exterior_scale):
+    mask = np.zeros(g.shape, bool)
+    candidate.fill_foreground(mask)
+    exterior_distance_map = ndi.distance_transform_edt(~mask) / exterior_scale
+    exterior_mask = np.logical_xor(mask, exterior_distance_map <= 10)
+    exterior_weights = np.zeros(g.shape)
+    exterior_weights[exterior_mask] = np.exp(-exterior_distance_map[exterior_mask])
+    exterior_mean = (g * exterior_weights).sum() / exterior_weights.sum()
+    interior_mean =  g[mask].mean()
+    return interior_mean - exterior_mean
+
+
 @ray.remote
-def _measure_candidate(cidx, candidate, params):
+def _process_candidate(cidx, candidate, params):
     region      = candidate.get_modelfit_region(params['y'], params['g_atoms'])
     energy_rate = candidate.energy / region.mask.sum()
+    contrast_response = _compute_contrast_response(candidate, params['g'], params['exterior_scale'])
+    fg_offset, fg_fragment = _process_mask(candidate, params['g_smooth'], params['mask_max_distance'], params['mask_stdamp'])
     return cidx, {
-        'energy_rate': energy_rate
+        'energy_rate':       energy_rate,
+        'contrast_response': contrast_response,
+        'fg_offset':         fg_offset,
+        'fg_fragment':       fg_fragment
     }
 
 
@@ -86,151 +120,25 @@ def _retain_intersections(superset_mask, subset_mask, copy=False):
     return result
 
 
-def _create_mask_postprocessor(g, max_distance, stdamp=1, smoothness=1):
+def _process_mask(candidate, g, max_distance, stdamp):
+    if stdamp <= 0 or max_distance <= 0:
+        return None, None
     mask = np.zeros(g.shape, bool)
-    g = ndi.gaussian_filter(g, smoothness)
-    def _process_mask(candidate):
-        candidate.fill_foreground(mask)
-        boundary_mask = np.logical_xor(morph.binary_dilation(mask, morph.disk(1)), morph.binary_erosion(mask, morph.disk(1)))
-        boundary_distance   = 1 + ndi.distance_transform_edt(~boundary_mask)
-        extra_mask_superset = (boundary_distance <= max_distance)
-        g_fg_data = g[mask]
-        fg_mean   = g_fg_data.mean()
-        fg_amp    = g_fg_data.std() * stdamp
-        extra_fg  = np.logical_and(fg_mean - fg_amp <= g, g <= fg_mean + fg_amp)
-        extra_bg  = np.logical_not(extra_fg)
-        extra_fg  = np.logical_and(extra_mask_superset, extra_fg)
-        extra_bg  = np.logical_and(extra_mask_superset, extra_bg)
-        extra_fg  = _retain_intersections(extra_fg, boundary_mask)
-        extra_bg  = _retain_intersections(extra_bg, boundary_mask)
-        mask[extra_fg] = True
-        mask[extra_bg] = False
-        assert mask.any()
-        fg_offset, fg_fragment = gocell.candidates.extract_foreground_fragment(mask)
-        fg_fragment = fg_fragment.copy()
-        mask.fill(False)
-        return fg_offset, fg_fragment
-    return _process_mask
-
-
-#import gocell.pipeline as pipeline
-#import gocell.config   as config
-#import gocell.surface  as surface
-#import gocell.aux      as aux
-#import numpy as np
-#import math
-#
-#from skimage.filters import threshold_otsu
-#from skimage         import morphology
-#from scipy           import ndimage
-#
-#
-#def threshold_accepted_energies(accepted_candidates, cfg):
-#    energies = np.array([c.energy for c in accepted_candidates])
-#    if len(energies) < 2: return np.inf
-#    t_otsu  = threshold_otsu(energies)
-#    t_gauss = energies.mean() + config.get_value(cfg, 'gauss_tolerance', 0.8) * energies.std()
-#    return max([t_otsu, t_gauss])
-#
-#
-#def compute_object_region_overlap(foreground_buf, candidate, g_superpixels):
-#    sel = candidate.fill_foreground(foreground_buf)
-#    roi_mask    = candidate.get_mask(g_superpixels)
-#    roi_overlap = np.logical_and(foreground_buf, roi_mask).sum() / float(foreground_buf.sum())
-#    foreground_buf[sel].fill(False)
-#    return roi_overlap
-#
-#
-#def compute_object_boundary(foreground_buf, candidate):
-#    sel = candidate.fill_foreground(foreground_buf)
-#    obj_interior = morphology.binary_erosion(foreground_buf, morphology.disk(1))
-#    obj_boundary = np.logical_xor(foreground_buf, obj_interior)
-#    foreground_buf[sel].fill(False)
-#    return obj_boundary.astype(bool)
-#
-#
-#class Postprocessing(pipeline.Stage):
-#
-#    def __init__(self):
-#        super(Postprocessing, self).__init__('postprocess',
-#                                             inputs  = ['g_raw', 'g_superpixels', 'accepted_candidates'],
-#                                             outputs = ['postprocessed_candidates'])
-#
-#    def process(self, input_data, cfg, out, log_root_dir):
-#        g_raw, g_superpixels, accepted_candidates = input_data['g_raw'], input_data['g_superpixels'], input_data['accepted_candidates']
-#        rejection_causes = {}
-#
-#        energy_threshold = threshold_accepted_energies(accepted_candidates, config.get_value(cfg, 'energy_threshold', {}))
-#        pp1_candidates = []
-#        for c in accepted_candidates:
-#            if c.energy < energy_threshold:
-#                pp1_candidates.append(c)
-#            else:
-#                rejection_causes[c] = 'maximum energy was %f but actual %f' % (energy_threshold, c.energy)
-#
-#        foreground_buf = np.zeros(g_raw.shape, bool)
-#        min_obj_region_overlap = config.get_value(cfg, 'min_obj_region_overlap', 0.5)
-#        pp2_candidates = []
-#        for c in pp1_candidates:
-#            region_overlap = compute_object_region_overlap(foreground_buf, c, g_superpixels)
-#            if region_overlap >= min_obj_region_overlap:
-#                pp2_candidates.append(c)
-#            else:
-#                rejection_causes[c] = 'min region overlap was %f but actual %f' % (min_obj_region_overlap, region_overlap)
-#
-#        r_map  = ndimage.filters.gaussian_gradient_magnitude(g_raw, config.get_value(cfg, 'r_sigma', 10.))
-#        r_map -= r_map.min()
-#        r_map /= r_map.max()
-#
-#        r_map_responses = {}
-#        r_map_response_func = {'mean': np.mean, 'median': np.median}[config.get_value(cfg, 'boundary_func', 'mean')]
-#        for c in pp2_candidates:
-#            cc = compute_object_boundary(foreground_buf, c)
-#            r_map_responses[c] = r_map_response_func(r_map[cc])
-#
-#        r_threshold = aux.threshold_gauss(r_map_responses.values(), mode='lower',
-#                                          tolerance=config.get_value(cfg, 'boundary_tolerance', np.inf))
-#        r_threshold = max([r_threshold,
-#                           max(r_map_responses.values()) * config.get_value(cfg, 'boundary_min', -np.inf)])
-#
-#        pp3_candidates = []
-#        for c in pp2_candidates:
-#            r_map_response = r_map_responses[c]
-#            if r_map_response >= r_threshold:
-#                pp3_candidates.append(c)
-#            else:
-#                rejection_causes[c] = 'minimum r_map response was %f but actual %f' % (r_threshold, r_map_response)
-#
-#        min_obj_radius = config.get_value(cfg, 'min_obj_radius', 0)
-#        max_obj_radius = config.get_value(cfg, 'max_obj_radius', np.inf)
-#        pp4_candidates = []
-#        pp4_shape = np.add(g_superpixels.shape, 2)
-#        for c in pp3_candidates:
-#            is_boundary_object = c.on_boundary
-#            obj_radius = math.sqrt(c.fg_fragment.sum() / math.pi)
-#            if obj_radius > max_obj_radius:
-#                rejection_causes[c] = 'radius (%s) too large (maximum is %s)' % (str(obj_radius), str(max_obj_radius))
-#            else:
-#                if not is_boundary_object and obj_radius < min_obj_radius:
-#                    rejection_causes[c] = 'radius (%s) too small (minimum is %s)' % (str(obj_radius), str(min_obj_radius))
-#                else:
-#                    if config.get_value(cfg, 'discard_image_boundary', False) and is_boundary_object:
-#                        rejection_causes[c] = 'intersects image exterior'
-#                    else:
-#                        pp4_candidates.append(c)
-#        
-#        if config.get_value(cfg, 'fill_holes', False):
-#            pp5_candidates = []
-#            for c in pp4_candidates:
-#                c = c.copy()
-#                c.fg_fragment = ndimage.morphology.binary_fill_holes(c.fg_fragment)
-#                pp5_candidates.append(c)
-#        else:
-#            pp5_candidates = pp4_candidates
-#
-#        candidate_indices = dict(zip(accepted_candidates, range(len(accepted_candidates))))
-#        self.rejection_causes = dict((candidate_indices[c], cause) for c, cause in rejection_causes.items())
-#        return {
-#            'postprocessed_candidates': pp5_candidates
-#        }
+    candidate.fill_foreground(mask)
+    boundary_mask = np.logical_xor(morph.binary_dilation(mask, morph.disk(1)), morph.binary_erosion(mask, morph.disk(1)))
+    boundary_distance   = 1 + ndi.distance_transform_edt(~boundary_mask)
+    extra_mask_superset = (boundary_distance <= max_distance)
+    g_fg_data = g[mask]
+    fg_mean   = g_fg_data.mean()
+    fg_amp    = g_fg_data.std() * stdamp
+    extra_fg  = np.logical_and(fg_mean - fg_amp <= g, g <= fg_mean + fg_amp)
+    extra_bg  = np.logical_not(extra_fg)
+    extra_fg  = np.logical_and(extra_mask_superset, extra_fg)
+    extra_bg  = np.logical_and(extra_mask_superset, extra_bg)
+    extra_fg  = _retain_intersections(extra_fg, boundary_mask)
+    extra_bg  = _retain_intersections(extra_bg, boundary_mask)
+    mask[extra_fg] = True
+    mask[extra_bg] = False
+    fg_offset, fg_fragment = gocell.candidates.extract_foreground_fragment(mask)
+    return fg_offset, fg_fragment
 
