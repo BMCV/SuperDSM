541ce5c771a873bd20ce8e9e131fc0ca0ae71104
Leonid Kostrykin <void@evoid.de>
2021-04-10 22:47:15 +0200

    adds `threshold_max` option for `preprocess`

--- gocell/preprocessing.py
+++ gocell/preprocessing.py
@@ -21,21 +21,26 @@ class PreprocessingStage(gocell.pipeline.Stage):
 
         sigma1 = gocell.config.get_value(cfg, 'sigma1', math.sqrt(2))
         sigma2 = gocell.config.get_value(cfg, 'sigma2', 40)
-        threshold_clip  = gocell.config.get_value(cfg, 'threshold_clip', 3)
+        threshold_clip = gocell.config.get_value(cfg, 'threshold_clip', 3)
+        threshold_max  = gocell.config.get_value(cfg, 'threshold_max' , None)
 
         threshold_original = ndi.gaussian_filter(g_raw, sigma2)
-        if np.isinf(threshold_clip):
-            threshold_combined = threshold_original
+        if threshold_max is None:
+            if np.isinf(threshold_clip):
+                threshold_combined = threshold_original
 
+            else:
+                threshold_clip_abs = threshold_clip * g_raw.std()
+                threshold_clipped  = ndi.gaussian_filter(g_raw.clip(0, threshold_clip_abs), sigma2)
+
+                clip_area = (g_raw > threshold_clip_abs)
+                _tmp1 = ndi.distance_transform_edt(~clip_area)
+                _tmp1 = (sigma2 - _tmp1).clip(0, np.inf)
+                _tmp1 = (_tmp1 / _tmp1.max()) ** 2
+                threshold_combined = (1 - _tmp1) * threshold_clipped + _tmp1 * threshold_original
+        
         else:
-            threshold_clip_abs = threshold_clip * g_raw.std()
-            threshold_clipped  = ndi.gaussian_filter(g_raw.clip(0, threshold_clip_abs), sigma2)
-
-            clip_area = (g_raw > threshold_clip_abs)
-            _tmp1 = ndi.distance_transform_edt(~clip_area)
-            _tmp1 = (sigma2 - _tmp1).clip(0, np.inf)
-            _tmp1 = (_tmp1 / _tmp1.max()) ** 2
-            threshold_combined = (1 - _tmp1) * threshold_clipped + _tmp1 * threshold_original
+            threshold_combined = ndi.maximum_filter(threshold_original, size=threshold_max * sigma2)
             
         if gocell.config.get_value(cfg, 'lower_clip_mean', False):
             threshold_combined = np.max([threshold_combined, np.full(g_raw.shape, g_raw.mean())], axis=0)
