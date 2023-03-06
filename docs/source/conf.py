54c743a451203cf7fa769a3bf3cdc7e8d6b0878b
Leonid Kostrykin <void@evoid.de>
2023-03-06 12:50:44 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 
 project = 'SuperDSM'
-copyright = '2018-2022 Leonid Kostrykin, Biomedical Computer Vision Group, Heidelberg University'
+copyright = '2018-2023 Leonid Kostrykin, Biomedical Computer Vision Group, Heidelberg University'
 author = 'Leonid Kostrykin'
 
 # -- General configuration
