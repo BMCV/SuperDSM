f00f86bdcfc5812b20a14427568bfd3c5714ae5d
Leonid Kostrykin <void@evoid.de>
2023-03-06 14:10:08 +0100

    fixes MKL stub

--- superdsm/_mkl.py
+++ superdsm/_mkl.py
@@ -2,7 +2,7 @@ try:
     from ._libs.sparse_dot_mkl import dot_product_mkl as dot, dot_product_transpose_mkl as gram
 
 except ImportError:
-    stub = lambda *args, **kwargs: raise NotImplementedError('Failed to load MKL')
+    def stub(*args, **kwargs): raise NotImplementedError('Failed to load MKL')
     dot  = stub
     gram = stub
 
