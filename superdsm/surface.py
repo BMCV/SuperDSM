93ca9cc1172b1c62900fb5c9a4d790d2bb099bda
Leonid Kostrykin <void@evoid.de>
2023-03-09 20:47:16 +0000

    removes dead code

--- superdsm/surface.py
+++ superdsm/surface.py
@@ -3,12 +3,6 @@ from .io import imread
 import numpy as np
 
 
-def get_pixel_map(shape, normalized=False):
-    z = (np.array(shape) - 1. if normalized else np.ones(2))[Ellipsis, None, None]
-    z[z == 0] = 1
-    return np.indices(shape) / z
-
-
 def bbox(mask, include_end=True):
     mask_a0 = mask.any(axis=0)
     mask_a1 = mask.any(axis=1)
@@ -24,10 +18,6 @@ class Surface:
         self.mask      = mask if mask is not None else np.ones(model.shape, bool)
         self.full_mask = full_mask if full_mask is not None else self.mask
         self.offset    = offset
-        
-    def shrink_mask(self, mask):
-        return mask[self.offset[0] : self.offset[0] + self.mask.shape[0],
-                    self.offset[1] : self.offset[1] + self.mask.shape[1]]
 
     def get_region(self, mask, shrink=False):
         mask = np.logical_and(self.mask, mask)
@@ -45,12 +35,3 @@ class Surface:
             if img_diff == 0: img_diff = 1
             img = (img - img.min()).astype(float) / img_diff
         return Surface(model=img, mask=mask)
-    
-    @staticmethod
-    def create_from_file(filepath):
-        img = imread(filepath)
-        return Surface.create_from_image(img)
-    
-    def get_map(self, normalized=True, pad=0):
-        assert pad >= 0 and isinstance(pad, int)
-        return get_pixel_map(np.add(self.model.shape, 2 * pad), normalized)
