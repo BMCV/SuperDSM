4ebdc665240683d0faa15ce179126dae5e95c343
Leonid Kostrykin <void@evoid.de>
2023-03-09 21:12:52 +0000

    drops the `preprocess/threshold_clip` option

--- superdsm/preprocess.py
+++ superdsm/preprocess.py
@@ -7,6 +7,24 @@ import numpy as np
 
 
 class Preprocessing(Stage):
+    """Implements the computation of the intensity offsets (see Supplemental Material 1 in :ref:`Kostrykin and Rohr, 2023 <references>`).
+
+    This stage requires ``g_raw`` for input (the input image) and produces ``y`` for output (the offset image intensities).
+
+    The following hyperparameters can be used to control this pipeline stage:
+
+    ``preprocess/sigma1``
+        The scale of the Gaussian filter used for denoising. Defaults to :math:`\\sqrt{2}`.
+
+    ``preprocess/sigma2``
+        The scale of the Gaussian filter :math:`\mathcal G_\sigma` used to determine the intensity offsets :math:`\\tau_x`. Defaults to :math:`40`.
+
+    ``preprocess/threshold_clip``
+        Corresponds to :math:`\\tau_\\text{max}` in Supplemental Material 1. Defaults to :math:`3`.
+
+    ``preprocess/lower_clip_mean``
+        If ``True``, intensity offsets :math:`\\tau_x` smaller than the mean image intensity are set to the mean image intensity. Defaults to ``False``.
+    """
 
     ENABLED_BY_DEFAULT = True
 
@@ -20,28 +38,24 @@ class Preprocessing(Stage):
 
         sigma1 = cfg.get('sigma1', math.sqrt(2))
         sigma2 = cfg.get('sigma2', 40)
-        threshold_clip = cfg.get('threshold_clip', 3)
-        threshold_max  = cfg.get('threshold_max' , None)
+        threshold_clip  = cfg.get('threshold_clip', 3)
+        lower_clip_mean = cfg.get('lower_clip_mean', False)
 
         threshold_original = ndi.gaussian_filter(g_raw, sigma2)
-        if threshold_max is None:
-            if np.isinf(threshold_clip):
-                threshold_combined = threshold_original
+        if np.isinf(threshold_clip):
+            threshold_combined = threshold_original
 
-            else:
-                threshold_clip_abs = threshold_clip * g_raw.std()
-                threshold_clipped  = ndi.gaussian_filter(g_raw.clip(0, threshold_clip_abs), sigma2)
-
-                clip_area = (g_raw > threshold_clip_abs)
-                _tmp1 = ndi.distance_transform_edt(~clip_area)
-                _tmp1 = (sigma2 - _tmp1).clip(0, np.inf)
-                _tmp1 = (_tmp1 / _tmp1.max()) ** 2
-                threshold_combined = (1 - _tmp1) * threshold_clipped + _tmp1 * threshold_original
-        
         else:
-            threshold_combined = ndi.maximum_filter(threshold_original, size=threshold_max * sigma2)
+            threshold_clip_abs = threshold_clip * g_raw.std()
+            threshold_clipped  = ndi.gaussian_filter(g_raw.clip(0, threshold_clip_abs), sigma2)
+
+            clip_area = (g_raw > threshold_clip_abs)
+            _tmp1 = ndi.distance_transform_edt(~clip_area)
+            _tmp1 = (sigma2 - _tmp1).clip(0, np.inf)
+            _tmp1 = (_tmp1 / _tmp1.max()) ** 2
+            threshold_combined = (1 - _tmp1) * threshold_clipped + _tmp1 * threshold_original
             
-        if cfg.get('lower_clip_mean', False):
+        if lower_clip_mean:
             threshold_combined = np.max([threshold_combined, np.full(g_raw.shape, g_raw.mean())], axis=0)
 
         y = ndi.gaussian_filter(g_raw, sigma1) - threshold_combined
