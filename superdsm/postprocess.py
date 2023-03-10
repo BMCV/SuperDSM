7305fc45bf8aea2962155b8c798209416c6c89ce
Leonid Kostrykin <void@evoid.de>
2023-03-10 13:21:00 +0000

    doc wip

--- superdsm/postprocess.py
+++ superdsm/postprocess.py
@@ -48,7 +48,7 @@ class Postprocessing(Stage):
 
     A segmented object is discarded, if the *contrast response* is too low, which is the ratio of *(i)* the mean image intensity inside the segmentation mask of the object and *(ii)* the average image intensity within its neighborhood. The average image intensity within the neighborhood is determined using a weighted mean of the image intensities, where the weight
 
-    .. math:: \\exp(-\\max\\{0, \\operatorname{dist}_M(x) - \\text{exterior\\_offset}\\} / \\text{exterior\\_scale})
+    .. math:: \\exp(-\\max\\{0, \\operatorname{dist}_M(x) - \\text{exterior\_offset}\\} / \\text{exterior\_scale})
         
     of an image point :math:`x` decays exponentially with the Euclidean distance :math:`\\operatorname{dist}_M(x)` of that point to the mask :math:`M` of the segmented object.
 
