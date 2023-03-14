ba13935ea4546b367e9862917c1a28b71af2e61f
Leonid Kostrykin <void@evoid.de>
2023-03-14 20:57:55 +0100

    doc wip

--- superdsm/image.py
+++ superdsm/image.py
@@ -4,12 +4,23 @@ import numpy as np
 
 
 def get_pixel_map(shape, normalized=False):
+    """Returns two 2D arrays corresponding to pixel coordinates of an array of the given shape.
+
+    The first array corresponds to the row indices (coordinates), the second array corresponds to the column indices (coordinates). The coordinates are normalized to the range between 0 and 1 if ``normalized`` is ``True``.
+
+    :return: The two 2D arrays, encapsulated as a single 3D array.
+    """
     z = (np.array(shape) - 1. if normalized else np.ones(2))[Ellipsis, None, None]
     z[z == 0] = 1
     return np.indices(shape) / z
 
 
 def bbox(mask, include_end=True):
+    """Returns the bounding box of a mask.
+
+    :param include_end: If ``True``, then the pair of last indices ``bbox[0][1]`` and ``bbox[1][1]`` is *included* in the specified ranges. It is *excluded* otherwise.
+    :return: Tuple ``(bbox, sel)``, where ``bbox[0]`` are the first and last indices of the rows and ``bbox[1]`` are the first and last indices of the columns, and ``sel`` is a numpy slice corresponding to that image region.
+    """
     mask_a0 = mask.any(axis=0)
     mask_a1 = mask.any(axis=1)
     ret = np.array([np.where(mask_a1)[0][[0, -1]], np.where(mask_a0)[0][[0, -1]]])
@@ -29,7 +40,7 @@ def normalize_image(img):
     return (img - img.min()).astype(float) / img_diff
 
 class Image:
-    """This class is used internally in SuperDSM to ease the work with images, image masks, and image regions.
+    """This class facilitates the work with images, image masks, and image regions.
     """
 
     def __init__(self, model=None, mask=None, full_mask=None, offset=(0,0)):
@@ -39,10 +50,18 @@ class Image:
         self.offset    = offset
 
     def shrink_mask(self, mask):
+        """Reduces a mask so it can be used to access this image.
+        """
         return mask[self.offset[0] : self.offset[0] + self.mask.shape[0],
                     self.offset[1] : self.offset[1] + self.mask.shape[1]]
 
     def get_region(self, mask, shrink=False):
+        """Returns the image region specified by a mask.
+        
+        :param mask: The binary mask used to specify the image region.
+        :param shrink: If ``True``, the image region will be reduced.
+        :return: The image region specified by the mask.
+        """
         mask = np.logical_and(self.mask, mask)
         if shrink:
             _bbox = bbox(mask)
@@ -52,10 +71,16 @@ class Image:
     
     @staticmethod
     def create_from_array(img, mask=None, normalize=True):
+        """Creates an instance from an image and a mask. 
+        """
         assert mask is None or (isinstance(mask, np.ndarray) and mask.dtype == bool)
         if normalize: img = normalize_image(img)
         return Image(model=img, mask=mask)
 
     def get_map(self, normalized=True, pad=0):
+        """Returns two 2D arrays corresponding to the pixel coordinates.
+
+        See :py:meth:`~.get_pixel_map` for details.
+        """
         assert pad >= 0 and isinstance(pad, int)
         return get_pixel_map(np.add(self.model.shape, 2 * pad), normalized)
