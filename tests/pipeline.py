13b1c95f7919ad53ab496176b3576ce2aaa131fa
Leonid Kostrykin <void@evoid.de>
2023-03-16 00:46:12 +0100

    tests wip

---
+++ tests/pipeline.py
@@ -0,0 +1,30 @@
+import unittest
+import ray
+import superdsm.automation, superdsm.io, superdsm.render
+
+from . import testsuite
+
+
+class pipeline(unittest.TestCase):
+
+    @testsuite.without_resource_warnings
+    def setUp(self):
+        ray.init(num_cpus=4, log_to_driver=False, logging_level=ray.logging.ERROR)
+        self.pipeline = superdsm.pipeline.create_default_pipeline()
+
+    @testsuite.without_resource_warnings
+    def tearDown(self):
+        ray.shutdown()
+        del self.pipeline
+
+#    def test_Pipeline_process_image(self):
+#        data_path = testsuite.require_data('bbbc033', 'C2.tif')
+#        img_3d = superdsm.io.imread(data_path)
+#        img = img_3d[28]
+#        data, _, _ = superdsm.automation.process_image(self.pipeline, superdsm.config.Config(), img)
+#        seg = superdsm.render.render_result_over_image(data, normalize_img=False)
+#        testsuite.validate_image(self, 'pipeline.Pipeline.process_image/bbbc033-z28.png', seg)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/render.py
+++ tests/render.py
@@ -7,23 +7,48 @@ from . import testsuite
 
 class render(unittest.TestCase):
 
+    @classmethod
     @testsuite.without_resource_warnings
-    def setUp(self):
+    def setUpClass(self):
         ray.init(num_cpus=4, log_to_driver=False, logging_level=ray.logging.ERROR)
-        self.pipeline = superdsm.pipeline.create_default_pipeline()
-
-    @testsuite.without_resource_warnings
-    def tearDown(self):
-        ray.shutdown()
-        del self.pipeline
-
-    def test_render_result_over_image(self):
-        data_path = testsuite.require_data('bbbc033', 'C2.tif')
         img_3d = superdsm.io.imread(data_path)
         img = img_3d[28]
-        data, _, _ = superdsm.automation.process_image(self.pipeline, superdsm.config.Config(), img)
-        seg = superdsm.render.render_result_over_image(data, normalize_img=False)
+        pipeline = superdsm.pipeline.create_default_pipeline()
+        data_path = testsuite.require_data('bbbc033', 'C2.tif')
+        self.data, _, _ = superdsm.automation.process_image(self.pipeline, superdsm.config.Config(), img)
+
+    @classmethod
+    @testsuite.without_resource_warnings
+    def tearDownClass(self):
+        ray.shutdown()
+
+    def test_render_result_over_image(self):
+        seg = superdsm.render.render_result_over_image(self.data, normalize_img=False)
         testsuite.validate_image(self, 'render.render_result_over_image/bbbc033-z28.png', seg)
+
+    def test_render_adjacencies(self):
+        seg = superdsm.render.render_adjacencies(self.data, normalize_img=False)
+        testsuite.validate_image(self, 'render.render_adjacencies/bbbc033-z28.png', seg)
+
+    def test_render_ymap(self):
+        seg = superdsm.render.render_ymap(self.data)
+        testsuite.validate_image(self, 'render.render_ymap/bbbc033-z28.png', seg)
+
+    def test_normalize_image(self):
+        seg = superdsm.render.normalize_image(self.data)
+        testsuite.validate_image(self, 'render.normalize_image/bbbc033-z28.png', seg)
+
+    def test_render_atoms(self):
+        seg = superdsm.render.render_atoms(self.data, normalize_img=False)
+        testsuite.validate_image(self, 'render.render_atoms/bbbc033-z28.png', seg)
+
+    def test_render_foreground_clusters(self):
+        seg = superdsm.render.render_foreground_clusters(self.data, normalize_img=False)
+        testsuite.validate_image(self, 'render.render_foreground_clusters/bbbc033-z28.png', seg)
+
+    def test_rasterize_labels(self):
+        seg = superdsm.render.rasterize_labels(self.data)
+        testsuite.validate_image(self, 'render.rasterize_labels/bbbc033-z28.png', seg)
 
 
 if __name__ == '__main__':
