4ca8fcdf837c4c8410921209c3c2196952cc7bf3
Leonid Kostrykin <void@evoid.de>
2023-03-15 21:21:17 +0100

    adds test suite

---
+++ tests/render.py
@@ -0,0 +1,28 @@
+import unittest
+import testsuite
+import numpy as np
+import ray
+import superdsm.automation, superdsm.io, superdsm.render
+
+
+class render(unittest.TestCase):
+
+    def setUp(self):
+        ray.init(num_cpus=4, log_to_driver=False, logging_level=ray.logging.ERROR)
+        self.pipeline = superdsm.pipeline.create_default_pipeline()
+
+    def tearDown(self):
+        ray.shutdown()
+        del self.pipeline
+
+    def test_render_result_over_image(self):
+        data_path = testsuite.require_data('bbbc033')
+        img_3d = superdsm.io.imread(data_path)
+        img = img_3d[28]
+        data, _, _ = superdsm.automation.process_image(self.pipeline, superdsm.config.Config(), img)
+        seg = superdsm.render.render_result_over_image(data, normalize_img=False)
+        testsuite.validate_image(self, 'render.render_result_over_image', seg)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/testsuite.py
@@ -0,0 +1,48 @@
+import os
+import pathlib
+import requests
+import tempfile
+import shutil
+import json
+import superdsm.io
+
+requests.packages.urllib3.disable_warnings()
+
+root_dir = pathlib.Path(os.path.realpath(__file__)).parent
+data_dir = root_dir / 'data'
+
+def load_data(url, datasets, **kwargs):
+    archive_suffix = ''.join(pathlib.Path(url).suffixes)
+    with tempfile.NamedTemporaryFile('wb', suffix=archive_suffix) as archive_file:
+        print(f'Downloading archive: {url}')
+        with requests.get(url, stream=True, **kwargs) as req:
+            req.raise_for_status()
+            for chunk in req.iter_content(chunk_size=10 * 1024 ** 2):
+                archive_file.write(chunk)
+        archive_file.flush()
+        with tempfile.TemporaryDirectory() as archive_dirpath:
+            print(f'Unpacking to {archive_dirpath}')
+            shutil.unpack_archive(archive_file.name, archive_dirpath)
+            src_root = pathlib.Path(archive_dirpath)
+            for src, dst in datasets:
+                print(f'Populating {data_dir / dst}')
+                shutil.move(str(src_root / src), str(data_dir / dst))
+
+def require_data(data_id):
+    data_path = data_dir / data_id
+    if not data_path.exists():
+        with open(data_dir / 'database.json') as dbfp:
+            db = json.load(dbfp)
+        data = db[data_id]
+        load_data(data['url'], data['datasets'])
+    return data_path
+
+
+def validate_image(test, name, img):
+    expected = superdsm.io.imread(root_dir / 'expected' / f'{name}.png')
+    try:
+        test.assertTrue(np.allclose(img, expected)) ## TODO: write "wrong" result to file if check fails
+    except:
+        actual_path = root_dir / 'actual'
+        actual_path.mkdir(exist_ok=True)
+        superdsm.io.imwrite(actual_path / f'{name}.png', img)
