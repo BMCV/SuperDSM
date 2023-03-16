3411fa026186a5462c85bee851e540d644ef101c
Leonid Kostrykin <void@evoid.de>
2023-03-16 18:05:11 +0100

    adds test for `superdsm.image.get_pixel_map`

---
+++ tests/test_image.py
@@ -0,0 +1,30 @@
+import unittest
+import numpy as np
+import superdsm.image
+
+from . import testsuite
+
+
+class image(unittest.TestCase):
+
+    def test_get_pixel_map(self):
+        actual1 = superdsm.image.get_pixel_map((5, 5))
+        actual2 = superdsm.image.get_pixel_map((5, 5), normalized=True)
+        expected = \
+         np.array([[[0., 0., 0., 0., 0.],
+                    [1., 1., 1., 1., 1.],
+                    [2., 2., 2., 2., 2.],
+                    [3., 3., 3., 3., 3.],
+                    [4., 4., 4., 4., 4.]],
+
+                   [[0., 1., 2., 3., 4.],
+                    [0., 1., 2., 3., 4.],
+                    [0., 1., 2., 3., 4.],
+                    [0., 1., 2., 3., 4.],
+                    [0., 1., 2., 3., 4.]]])
+        self.assertEqual(actual1, expected)
+        self.assertEqual(actual2, expected / 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
