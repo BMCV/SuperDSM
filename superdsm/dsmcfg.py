795b1afd6a64f6ea853e935749ad3f9157e3de7b
Leonid Kostrykin <void@evoid.de>
2023-03-20 23:04:46 +0100

    doc wip

--- superdsm/dsmcfg.py
+++ superdsm/dsmcfg.py
@@ -68,7 +68,7 @@ class DSM_Config(Stage):
         Maximum number of simultaneous allocation of the matrix :math:`\\tilde G_\\omega` during parallel processing (see Section 3.3 of the :ref:`paper <references>`, each allocation might require a considerable amount of system memory).
 
     ``dsm/background_margin``
-        Governs the amount of image background included in the obtained image region. This is the width of the "stripe" of background retained around each connected foreground region (in pixels). See Supplemental Material 6 of the :ref:`paper <references>` for details, however, due to a transmission error, the threshold :math:`\\sigma_G` in Eq. (S11) was misstated by a factor of 2 (the correct threshold is :math:`2\\sigma_G`).
+        Governs the amount of image background included in the obtained image region. This is the width of the "stripe" of background retained around each connected foreground region (in pixels). See Supplemental Material 6 of the :ref:`paper <references>` for details, however, due to a transmission error, the threshold :math:`\\sigma_G` in Eq. (S11) was misstated by a factor of 2 (the correct threshold is :math:`2\\sigma_G`). Defaults to 20, or to ``AF_background_margin × scale`` if computed automatically (forced to :math:`\\geq 8` and ``AF_background_margin`` defaults to 0.4).
 
     ``dsm/cp_timeout``
         The maximum run time of convex programming for each object (in seconds). The convex optimization will be interrupted if it takes longer than that (the :py:meth:`~superdsm.objects.cvxprog` function will report the status ``fallback`` in this case). Defaults to 300 (i.e. 5 minutes).
@@ -89,11 +89,10 @@ class DSM_Config(Stage):
         }
 
     def configure_ex(self, scale, radius, diameter):
-        smooth_subsample_spec = (scale, 0.4, dict(type=int, min=8))
         return {
             'alpha': (scale ** 2, 0.0005),
-            'smooth_amount': (scale, 0.2, dict(type=int, min=4)),
-            'smooth_subsample': smooth_subsample_spec,
-            'background_margin': smooth_subsample_spec,
+            'smooth_amount':     (scale, 0.2, dict(type=int, min=4)),
+            'smooth_subsample':  (scale, 0.4, dict(type=int, min=8)),
+            'background_margin': (scale, 0.4, dict(type=int, min=8)),
         }
 
