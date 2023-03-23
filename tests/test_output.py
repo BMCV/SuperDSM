294f6352b6d7b2bdf12e3a27d26c00c3e712f7b8
Leonid Kostrykin <void@evoid.de>
2023-03-23 01:43:42 +0100

    wip on tests

--- tests/test_output.py
+++ tests/test_output.py
@@ -9,13 +9,13 @@ class output(unittest.TestCase):
 
     def test_get_output(self):
         out1 = superdsm.output.get_output(None)
-        self.assertIsInstance(out1, superdsm.output.Output)
+        self.assertIsInstance(out1, superdsm.output.Output, f'Actual type: {type(out1)}')
         self.assertFalse(out1.muted)
         out2 = superdsm.output.get_output(out1)
-        self.assertIsInstance(out2, superdsm.output.Output)
+        self.assertIsInstance(out2, superdsm.output.Output, f'Actual type: {type(out2)}')
         self.assertIs(out1, out2)
         out3 = superdsm.output.get_output('muted')
-        self.assertIsInstance(out3, superdsm.output.Output)
+        self.assertIsInstance(out3, superdsm.output.Output, f'Actual type: {type(out3)}')
         self.assertTrue(out3.muted)
 
 
