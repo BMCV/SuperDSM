d391fbc3be210d40f8122736b49e49c6ee4261c6
Leonid Kostrykin <void@evoid.de>
2023-03-23 01:42:25 +0100

    fixes tests

--- tests/test_output.py
+++ tests/test_output.py
@@ -9,10 +9,13 @@ class output(unittest.TestCase):
 
     def test_get_output(self):
         out1 = superdsm.output.get_output(None)
+        self.assertIsInstance(out1, superdsm.output.Output)
         self.assertFalse(out1.muted)
         out2 = superdsm.output.get_output(out1)
+        self.assertIsInstance(out2, superdsm.output.Output)
         self.assertIs(out1, out2)
         out3 = superdsm.output.get_output('muted')
+        self.assertIsInstance(out3, superdsm.output.Output)
         self.assertTrue(out3.muted)
 
 
