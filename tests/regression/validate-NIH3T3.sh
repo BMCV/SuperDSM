d47fb026f53a23d5dd95be85dd8d93b3ffee6e97
Leonid Kostrykin <void@evoid.de>
2024-02-17 16:44:10 +0100

    Fix regression tests

--- tests/regression/validate-NIH3T3.sh
+++ tests/regression/validate-NIH3T3.sh
@@ -5,10 +5,10 @@ set -e
 for taskdir in "NIH3T3/default" "NIH3T3/default/adapted" "NIH3T3/default/adapted/isbi24"
 do
 
-    mkdir -p "$1/$(taskdir)"
+    mkdir -p "$1/${taskdir}"
     python tests/regression/validate.py \
-      "examples/$(taskdir)/seg" \
-      "$1/$(taskdir)" \
-      "tests/regression/expected/$(hostname)/$(taskdir)"
+      "examples/${taskdir}/seg" \
+      "$1/${taskdir}" \
+      "tests/regression/expected/$(hostname)/${taskdir}"
 
 done
--- tests/regression/validate-U2OS.sh
+++ tests/regression/validate-U2OS.sh
@@ -5,10 +5,10 @@ set -e
 for taskdir in "U2OS/default" "U2OS/default/adapted" "U2OS/default/adapted/isbi24"
 do
 
-    mkdir -p "$1/$(taskdir)"
+    mkdir -p "$1/${taskdir}"
     python tests/regression/validate.py \
-      "examples/$(taskdir)/seg" \
-      "$1/$(taskdir)" \
-      "tests/regression/expected/$(hostname)/$(taskdir)"
+      "examples/${taskdir}/seg" \
+      "$1/${taskdir}" \
+      "tests/regression/expected/$(hostname)/${taskdir}"
 
 done
